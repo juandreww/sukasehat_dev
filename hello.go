2621c6dc7966e6c51143fc53492b1b0e6dc09760
juandreww <juandreww@outlook.com>
2021-09-03 07:39:05 +0700

    literalsd

--- hello.go
+++ hello.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world")
 }
+
+/*
+	Literals:
+	// 54 is Decimal
+	// 0113 is Octal
+	// 0x4a is hexadecimal
+	// 40 is int
+	// 40u is unsigned int
+	// 40l is long
+	// 40ul is unsigned long
+*/
