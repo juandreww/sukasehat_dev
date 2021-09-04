0b4f7649c1c861621f50791ba4eaf2402efdf1f8
juandreww <juandreww@outlook.com>
2021-09-04 10:49:39 +0700

    hello go

--- hello.go
+++ hello.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var new int = 1234
-	fmt.Println(new)
+	var new int = 1_234
+	var x float64 = 10.6
+	var y int = new + int(x)
+	fmt.Println(y)
 }
 
 /*
 	Literals:
-	// 54 is Decimal
-	// 0113 is Octal
+	// 0d54 is Decimal
+	// 0o113 is Octal
 	// 0x4a is hexadecimal
 	// 40 is int
 	// 40u is unsigned int
