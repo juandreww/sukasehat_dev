af14c1c8d58593093f2e03b6d50ecb47d4769a7b
juandreww <juandreww@outlook.com>
2021-09-11 22:29:52 +0700

    helloGo

--- hello.go
+++ hello.go
@@ -14,12 +14,13 @@ const (
 func main() {
 	x := make([]int, 0, 5)
 x = append(x, 1, 2, 3, 4)
-y := x[:2]
-z := x[2:]
+y := x[:2:2]
+z := x[2:4:4]
 fmt.Println(cap(x), cap(y), cap(z))
 y = append(y, 30, 40, 50)
 x = append(x, 60)
 z = append(z, 70)
+
 fmt.Println("x:", x)
 fmt.Println("y:", y)
 fmt.Println("z:", z)
