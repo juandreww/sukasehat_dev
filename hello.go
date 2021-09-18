fdf1971581340455d0567e21a4fcb202a4c20145
juandreww <juandreww@outlook.com>
2021-09-18 15:31:26 +0700

    HelloGo

--- hello.go
+++ hello.go
@@ -14,12 +14,20 @@ const (
 // var i []int
 
 func main() {
-	evenVals := []int{2, 4, 6, 8, 10}
-	for i := 1; i < len(evenVals)-1; i++ {
-		fmt.Println(i, evenVals[i])
+	words := []string{"a", "cow", "smile", "gopher",
+	"octopus", "anthropologist"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "is exactly the right length:", wordLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word!")
+		}
 	}
-
-	
 }
 
 /*
