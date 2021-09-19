8006f455d9433ffeb9b696dd0292ba2061c45d6d
juandreww <juandreww@outlook.com>
2021-09-19 21:08:36 +0700

    Go Calcualtor

--- hello.go
+++ hello.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
-import "errors"
+import "strconv"
+// import "errors"
 // import "math/rand"
 
 const (
@@ -9,27 +10,52 @@ const (
 	nameKey = "name"
 )
 
-func addTo(base int, vals ...int) []int {
-	out := make([]int, 0, len(vals))
-	for _, v := range vals {
-	out = append(out, base+v)
-	}
-	return out
-}
+func add(i int, j int) int { return i + j }
+func sub(i int, j int) int { return i - j }
+func mul(i int, j int) int { return i * j }
+func div(i int, j int) int { return i / j }
 
-func divAndRemainder(numerator int, denominator int) (result int, remainder int,
-	err error) {
-	if denominator == 0 {
-	err = errors.New("cannot divide by zero")
-	return result, remainder, err
-	}
-	result, remainder = numerator/denominator, numerator%denominator
-	return result, remainder, err
+var opMap = map[string]func(int, int) int{
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
 }
 
 func main() {
-	x, y, z := divAndRemainder(5, 2)
-	fmt.Println(x, y, z)
+	expressions := [][]string{
+		[]string{"2", "+", "3"},
+		[]string{"2", "-", "3"},
+		[]string{"2", "*", "3"},
+		[]string{"2", "/", "3"},
+		[]string{"2", "%", "3"},
+		[]string{"two", "+", "three"},
+		[]string{"5"},
+	}
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("invalid expression:", expression)
+			continue
+	}
+	p1, err := strconv.Atoi(expression[0])
+	if err != nil {
+		fmt.Println(err)
+		continue
+	}
+	op := expression[1]
+	opFunc, ok := opMap[op]
+	if !ok {
+		fmt.Println("unsupported operator:", op)
+		continue
+	}
+	p2, err := strconv.Atoi(expression[2])
+	if err != nil {
+		fmt.Println(err)
+		continue
+	}
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
 }
 
 /*
