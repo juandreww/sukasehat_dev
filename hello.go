bfbcaefa956782431d0a9db1fec486bfc63a2aec
juandreww <juandreww@outlook.com>
2021-09-19 22:26:26 +0700

    helloGo

--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "strconv"
+// import "strconv"
 // import "errors"
 // import "math/rand"
 
@@ -23,38 +23,10 @@ var opMap = map[string]func(int, int) int{
 }
 
 func main() {
-	expressions := [][]string{
-		[]string{"2", "+", "3"},
-		[]string{"2", "-", "3"},
-		[]string{"2", "*", "3"},
-		[]string{"2", "/", "3"},
-		[]string{"2", "%", "3"},
-		[]string{"two", "+", "three"},
-		[]string{"5"},
-	}
-	for _, expression := range expressions {
-		if len(expression) != 3 {
-			fmt.Println("invalid expression:", expression)
-			continue
-	}
-	p1, err := strconv.Atoi(expression[0])
-	if err != nil {
-		fmt.Println(err)
-		continue
-	}
-	op := expression[1]
-	opFunc, ok := opMap[op]
-	if !ok {
-		fmt.Println("unsupported operator:", op)
-		continue
-	}
-	p2, err := strconv.Atoi(expression[2])
-	if err != nil {
-		fmt.Println(err)
-		continue
-	}
-		result := opFunc(p1, p2)
-		fmt.Println(result)
+	for i := 0; i < 5; i++ {
+		func(j int) {
+			fmt.Println("printing", j, "from inside of an anonymous function")
+		}(i)
 	}
 }
 
