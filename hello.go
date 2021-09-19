76e0f53ddc4ec68fb7d185fb18d6c758f9d0a544
juandreww <juandreww@outlook.com>
2021-09-19 21:01:02 +0700

    HelloGo

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "errors"
 // import "math/rand"
 
 const (
@@ -16,15 +17,21 @@ func addTo(base int, vals ...int) []int {
 	return out
 }
 
-func main() {
-	fmt.Println(addTo(3))
-	fmt.Println(addTo(3, 2))
-	fmt.Println(addTo(3, 2, 4, 6, 8))
-	a := []int{4, 3}
-	fmt.Println(addTo(3, a...))
-	fmt.Println(addTo(3, []int{1, 2, 3, 4, 5}...))
+func divAndRemainder(numerator int, denominator int) (result int, remainder int,
+	err error) {
+	if denominator == 0 {
+	err = errors.New("cannot divide by zero")
+	return result, remainder, err
+	}
+	result, remainder = numerator/denominator, numerator%denominator
+	return result, remainder, err
 }
-//
+
+func main() {
+	x, y, z := divAndRemainder(5, 2)
+	fmt.Println(x, y, z)
+}
+
 /*
 	Literals:
 	// 0d54 is Decimal
