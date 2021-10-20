542674ed8a083d896f2c819018ceec7caf0c8edd
juandreww <juandreww@outlook.com>
2021-10-20 14:07:50 +0700

    helloGo

--- hello.go
+++ hello.go
@@ -4,14 +4,10 @@ type Point struct{ X, Y float64 }
 
 // traditional function
 func Distance(p, q Point) float64 {
-return math.Hypot(q.Xp.
-X, q.Yp.
-Y)
+	return math.Hypot(q.Xp.X, q.Yp.Y)
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-return math.Hypot(q.Xp.
-X, q.Yp.
-Y)
+	return math.Hypot(q.Xp.X, q.Yp.Y)
 }
