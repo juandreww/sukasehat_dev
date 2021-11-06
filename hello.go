635c60b7a8c7e2f73c456a2317c16ee930469133
juandreww <juandreww@outlook.com>
2021-11-06 22:29:26 +0700

    wear shades

--- hello.go
+++ hello.go
@@ -10,4 +10,12 @@ func main() {
 	var command = "walk outside"
 	var exit = strings.Contains(command, "outside")
 	fmt.Println("You leave the cave:", exit)
+
+	var sunshine = "Emerging from the cave, your eyes meet the midday sun"
+	var wearShades = strings.Contains(sunshine, "midday sun")
+	fmt.Println("You need to wear a shades:", wearShades)
+
+	var walkingInsideTheCave = "Walking inside the cave. You see a beautiful sign. You decided to read"
+	var readSign = strings.Contains(walkingInsideTheCave, "read")
+	fmt.Println("Your decision is to read the sign:", readSign)
 }
