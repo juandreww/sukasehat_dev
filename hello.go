3f2149242529a0268c42e11c3d73200f9e33d391
juandreww <juandreww@outlook.com>
2021-11-06 22:40:18 +0700

    we are at the mountain

--- hello.go
+++ hello.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	fmt.Println("You find yourself in a dimly lit cavern.")
-	var command = "walk outside"
-	var exit = strings.Contains(command, "outside")
-	fmt.Println("You leave the cave:", exit)
-
-	var sunshine = "Emerging from the cave, your eyes meet the midday sun"
-	var wearShades = strings.Contains(sunshine, "midday sun")
-	fmt.Println("You need to wear a shades:", wearShades)
-
-	var walkingInsideTheCave = "Walking inside the cave. You see a beautiful sign. You decided to read"
-	var readSign = strings.Contains(walkingInsideTheCave, "read")
-	fmt.Println("Your decision is to read the sign:", readSign)
+	var destination = "Mountain"
+	
+	if destination == "Mountain" {
+		fmt.Printf("We are at the Mountain\n")
+	} else if destination == "Cave" {
+		fmt.Printf("We are at the Cave\n")
+	} else {
+		fmt.Printf("We are at the Entrance\n")
+	}
 }
