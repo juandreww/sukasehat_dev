1c0b5967812bf0e4c345ab7439a416cbff7a7c8c
juandreww <juandreww@outlook.com>
2021-11-07 16:33:45 +0700

    current random number

--- hello.go
+++ hello.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	var count = 10
-	for count > 0 {
-		fmt.Println(count)
-		time.Sleep(time.Second)
-		count--
-		var randm = rand.Intn(4)
-		fmt.Printf("Facing obstacles at %v\n", randm)
-		if randm == 0 {
-			fmt.Printf("Program stops at %v\n", randm)
-			break
+	var myNumber = 19
+	for {
+		var randomNumber = rand.Intn(25)
+		fmt.Printf("The current random number is: %v\n", randomNumber)
+		if randomNumber == myNumber {
+			break;
 		}
+		time.Sleep(time.Second)
 	}
-	fmt.Println("Liftoff!")
+	fmt.Println("You have found it.")
 }
