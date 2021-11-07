72de34a247aa221a79a8ed034c68d19848e1b337
juandreww <juandreww@outlook.com>
2021-11-07 16:28:24 +0700

    random int

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func main() {
@@ -11,6 +12,12 @@ func main() {
 		fmt.Println(count)
 		time.Sleep(time.Second)
 		count--
+		var randm = rand.Intn(4)
+		fmt.Printf("Facing obstacles at %v\n", randm)
+		if randm == 0 {
+			fmt.Printf("Program stops at %v\n", randm)
+			break
+		}
 	}
 	fmt.Println("Liftoff!")
 }
