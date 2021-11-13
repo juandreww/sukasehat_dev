6349ea1ff3c7212b1b5166e1df4e1c9ba4807ea5
juandreww <juandreww@outlook.com>
2021-11-13 23:04:47 +0700

    landing to mars

--- hello.go
+++ hello.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
+var era = "AD"
+
 func main() {
-	var count = 0
-	for count = 10; count > 0; count-- {
-		fmt.Println(count)
+	year := 2018
+	switch month := rand.Intn(12) + 1; month {
+	case 2:
+		day := rand.Intn(28) + 1
+		fmt.Println(era, year, month, day)
+	case 4, 6, 9, 11:
+		day := rand.Intn(30) + 1
+		fmt.Println(era, year, month, day)
+	default:
+		day := rand.Intn(31) + 1
+		fmt.Println(era, year, month, day)
 	}
-	fmt.Println(count)
 }
