3eaef120a2ae500fdeebb886a342b6434a240360
juandreww <juandreww@outlook.com>
2021-11-16 22:06:12 +0700

    first big project

--- hello.go
+++ hello.go
@@ -3,26 +3,46 @@ package main
 import (
   "fmt"
   "math/rand"
+  "time"
 )
 
-var era = "AD"
-
 func main() {
+  fmt.Printf("Spaceline \t\t Days \t Type \t\t Price \t Number of Days \t Departure Date \t\t Arrival Date \t\t\t Total Cost\n")
+  fmt.Printf("=================================================================================================================================================================\n")
   for i := 1; i <= 10; i++ {
-    year := rand.Intn(20) + 2010
-    month := rand.Intn(12) + 1
-    daysInMonth := 31
-    switch month {
+    var intSpaceline int = rand.Intn(3) + 1
+    var daysSpent int = rand.Intn(20) + 20
+    var singleOrRound int = rand.Intn(2) + 1
+
+    spaceline := ""
+    countTrip := ""
+
+    switch intSpaceline {
+    case 1:
+      spaceline = "SpaceX\t\t"
     case 2:
-      if year%4 == 0 {
-        daysInMonth = 29
-      } else {
-        daysInMonth = 28
-      }
-    case 4, 6, 9, 11:
-      daysInMonth = 30
+      spaceline = "Virgin Galactic"
+    case 3:
+      spaceline = "Space Adventures"
     }
-    day := rand.Intn(daysInMonth) + 1
-    fmt.Println(era, year, month, day)
+
+    switch singleOrRound {
+    case 1:
+      countTrip = "One-way"
+    case 2:
+      countTrip = "Round-trip"
+    }
+
+    var distanceToMars int = 62100000
+    var baseSpeedSpaceline int = rand.Intn(15)
+    var speedSpaceline int =  baseSpeedSpaceline  + 16
+    var baseCostSpaceline int = 35 + baseSpeedSpaceline
+    secondSpent := distanceToMars / speedSpaceline / 216000
+    costSpaceline := baseCostSpaceline * singleOrRound
+    departure := time.Date(2020, 10, 13, 0, 0, 0, 0, time.UTC)
+    arrival := departure.AddDate(0, 0, secondSpent)
+
+    fmt.Printf("%v \t %v \t %v \t $S %v \t %v \t\t\t %v \t %v \t $S %v\n", spaceline, daysSpent, countTrip, baseCostSpaceline , secondSpent, departure, arrival, costSpaceline)
+
   }
 }
