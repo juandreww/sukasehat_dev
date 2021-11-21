fe24936311655bd718488900852dde6410ed19ed
juandreww <juandreww@outlook.com>
2021-11-21 19:49:54 +0700

    floating error

--- hello.go
+++ hello.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	third := 1.0 / 3.0
-	fmt.Println(third + third + third)
-	piggyBank := 0.1
-	piggyBank += 0.2
+	piggyBank := 0.0
+	for i := 0; i < 11; i++ {
+		piggyBank += 0.1
+	}
 	fmt.Println(piggyBank)
 }
