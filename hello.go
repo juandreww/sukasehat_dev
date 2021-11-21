9eafb4d4e2eb5745785b4b3e52b2d2f72377ec58
juandreww <juandreww@outlook.com>
2021-11-21 21:58:25 +0700

    nickels dimes quarters

--- hello.go
+++ hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
+
+	nickels := 0.05
+	dimes := 0.1
+	quarters := 0.25
 	piggyBank := 0.0
-	for i := 0; i < 11; i++ {
-		piggyBank += 0.1
+
+	for piggyBank <= 20 {
+		num := rand.Intn(3) + 1
+		print(num)
+		if num == 1 {
+			piggyBank += nickels
+		} else if num == 2 {
+			piggyBank += quarters
+		} else if num == 3 {
+			piggyBank += dimes
+		}
+		fmt.Printf("Current Bank: %.8f\n", piggyBank)
 	}
-	fmt.Println(piggyBank)
 }
