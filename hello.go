6feae4ac692da215a21d221f1caeee42b729887b
juandreww <juandreww@outlook.com>
2021-11-22 21:08:48 +0700

    invoice type

--- hello.go
+++ hello.go
@@ -2,26 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func main() {
-
-	nickels := 0.05
-	dimes := 0.1
-	quarters := 0.25
-	piggyBank := 0.0
-
-	for piggyBank <= 20 {
-		num := rand.Intn(3) + 1
-		print(num)
-		if num == 1 {
-			piggyBank += nickels
-		} else if num == 2 {
-			piggyBank += quarters
-		} else if num == 3 {
-			piggyBank += dimes
-		}
-		fmt.Printf("Current Bank: %.8f\n", piggyBank)
-	}
+	year := 2018
+	fmt.Printf("Type %T for %v\n", year, year)
 }
