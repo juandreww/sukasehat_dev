5a196002f7e484e9016509ef5f2e9d02bc84cb74
juandreww <juandreww@outlook.com>
2021-11-23 22:53:46 +0700

    red green blue

--- hello.go
+++ hello.go
@@ -1,10 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	year := 2018
-	fmt.Printf("Type %T for %v\n", year, year)
+	var red, green, blue uint8 = 0x00, 0x8d, 0xd5
+
+	fmt.Printf("%x %x %x\n", red, green, blue)
+	fmt.Printf("color: #%02x%02x%02x;\n", red, green, blue)
+
+	var blood uint8 = 255
+	red++
+	fmt.Println(blood)
+	var number int8 = 127
+	number++
+	fmt.Println(number)
 }
