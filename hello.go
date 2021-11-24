c806d15a191f76924c5f9fce3940a53920913ac4
juandreww <juandreww@outlook.com>
2021-11-24 22:31:35 +0700

    1 binary addition

--- hello.go
+++ hello.go
@@ -1,17 +1,11 @@
+// You can edit this code!
+// Click here and start typing.
 package main
 
 import "fmt"
 
 func main() {
-	var red, green, blue uint8 = 0x00, 0x8d, 0xd5
-
-	fmt.Printf("%x %x %x\n", red, green, blue)
-	fmt.Printf("color: #%02x%02x%02x;\n", red, green, blue)
-
-	var blood uint8 = 255
+	var red uint16 = 65535
 	red++
-	fmt.Println(blood)
-	var number int8 = 127
-	number++
-	fmt.Println(number)
+	fmt.Println(red)
 }
