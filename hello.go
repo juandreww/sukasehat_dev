4490b912b4d6bc6a244729d3086ecbac931f97dd
juandreww <juandreww@outlook.com>
2021-11-27 22:03:25 +0700

    Canis Major Dwarf

--- hello.go
+++ hello.go
@@ -1,20 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math/big"
-)
+import "fmt"
 
 func main() {
-	lightSpeed := big.NewInt(299792)
-	secondsPerDay := big.NewInt(86400)
-	distance := new(big.Int)
-	distance.SetString("24000000000000000000", 10)
-	fmt.Println("Andromeda Galaxy is", distance, "km away.")
-
-	seconds := new(big.Int)
-	seconds.Div(distance, lightSpeed)
-	days := new(big.Int)
-	days.Div(seconds, secondsPerDay)
-	fmt.Println("That is", days, "days of travel at light speed.")
+	const distance = 236000000000000000
+	const lightSpeed = 299792 * 3600
+	const secondsPerDay = 86400
+	const days = distance / lightSpeed / secondsPerDay
+	fmt.Println("Canis Major Dwarf is", days, "light years away.")
 }
+
