445e856c48ba9aa2ee3badfdf67036cad292b5ec
juandreww <juandreww@outlook.com>
2021-11-28 19:49:27 +0700

    ascii manipulation

--- hello.go
+++ hello.go
@@ -5,7 +5,17 @@ package main
 import "fmt"
 
 func main() {
-	c := 'a'
-	c = c + 3
-	fmt.Printf("%c", c)
+	message := "uv vagreangvbany fcnpr fgngvba"
+	for i := 0; i < len(message); i++ {
+		c := message[i]
+		if c >= 'a' && c <= 'z' {
+			c = c + 13
+			if c > 'z' {
+				c = c - 26
+			}
+		}
+		fmt.Printf("%v : %c \n", c, c)
+		z := 'z' + 400
+		fmt.Printf("%v : %c \n", z, z)
+	}
 }
