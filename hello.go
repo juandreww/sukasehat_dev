05d4c892382b0d50fffb5190ae91114d6df38bdb
juandreww <juandreww@outlook.com>
2021-11-28 14:15:50 +0700

    peace string literals

--- hello.go
+++ hello.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const distance = 236000000000000000
-	const lightSpeed = 299792 * 3600
-	const secondsPerDay = 86400
-	const days = distance / lightSpeed / secondsPerDay
-	fmt.Println("Canis Major Dwarf is", days, "light years away.")
+	peace := "peace"
+	var peace = "peace"
+	var peace string = "peace"
+	fmt.println(peace)
 }
 
