d8ea7d4b2b974e4a79fae11892f8bb1e08079e27
juandreww <juandreww@outlook.com>
2021-11-28 20:28:02 +0700

    type dont mix

--- hello.go
+++ hello.go
@@ -5,20 +5,6 @@ import (
 )
 
 func main() {
-	question := "L fdph, L vdz, L frqtxhuhg"
-	for i := 0; i < len(question); i++ {
-		c := question[i]
-		if c >= 'A' && c <= 'Z' {
-			c = c - 3
-			if c > 'Z' {
-				c = c - 26
-			}
-		} else if c >= 'a' && c <= 'z' {
-			c = c - 3
-			if c > 'z' {
-				c = c - 26
-			}
-		}
-		fmt.Printf("%c", c)
-	}
+	countdown := "Launch in T minus " + "10 seconds."
+	fmt.Println(countdown)
 }
