7acece5623f9288b3289387740ef9db411bc2f4a
juandreww <juandreww@outlook.com>
2021-12-01 22:44:27 +0700

    my mars age is 13 years old

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	countdown := "Launch in T minus " + "10 seconds."
-	fmt.Println(countdown)
+	age := 25
+	marsAge := float64(age)
+	marsDays := 687.0
+	earthDays := 365.2425
+	marsAge = marsAge * earthDays / marsDays
+	fmt.Println("I am", marsAge, "years old on Mars.")
 }
