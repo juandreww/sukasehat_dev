6fe056c26716ee50697a38d564d97de397520e34
juandreww <juandreww@outlook.com>
2021-12-01 22:59:36 +0700

    mismatched type

--- hello.go
+++ hello.go
@@ -9,4 +9,7 @@ func main() {
 	earthDays := 365.2425
 	marsAge = marsAge * earthDays / marsDays
 	fmt.Println("I am", marsAge, "years old on Mars.")
+
+	check := age > marsAge
+	fmt.Println(check)
 }
