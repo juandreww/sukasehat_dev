e1195a8122ba65c4eca9c6c190ba884ecc41e2be
juandreww <juandreww@outlook.com>
2021-12-13 22:47:19 +0700

    decipher cipher vigenere cipher

--- hello.go
+++ hello.go
@@ -8,20 +8,38 @@ import (
 )
 
 func main() {
-	cipherText := "CSOITEUIWUIZNSROCNKFD"
+	plainText := "your message goes here"
 	keyword := "GOLANG"
-	cipherLen := len(cipherText) / len(keyword)
+
+	newString := strings.ToUpper(strings.Replace(plainText, " ", "", -1))
+	cipherLen := len(newString) / len(keyword)
 	stringx := strings.Repeat(keyword, cipherLen)
+
+	fmt.Println(newString)
 	count := 0
 	var result string
 	for _, c := range stringx {
-		merje := int32(cipherText[count]) + c - 65
-		if merje > 90 {
-			merje -= 25
+		merje := int32(newString[count]) + 65 - c
+		if merje <= 65 {
+			merje += 25
 		}
 		newchar := string(merje)
 		result = result + newchar
 		count++
 	}
 	fmt.Println(result)
+
+	count = 0
+	var result2 string
+	for _, c := range stringx {
+		merje := int32(result[count]) + c - 65
+		if merje >= 90 {
+			merje -= 25
+		}
+		newchar := string(merje)
+		result2 = result2 + newchar
+		count++
+	}
+
+	fmt.Println(result2)
 }
