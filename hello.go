18c59ace5b8b82b149d5acae78f78ccabb118aac
juandreww <juandreww@outlook.com>
2021-12-13 22:25:46 +0700

    decipher go

--- hello.go
+++ hello.go
@@ -2,16 +2,26 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	string := "true"
-
-	switch string {
-	case "yes", "true", "1":
-		fmt.Println("String is true")
-	case "no", "false", "0":
-		fmt.Println("String is false")
+	cipherText := "CSOITEUIWUIZNSROCNKFD"
+	keyword := "GOLANG"
+	cipherLen := len(cipherText) / len(keyword)
+	stringx := strings.Repeat(keyword, cipherLen)
+	count := 0
+	var result string
+	for _, c := range stringx {
+		merje := int32(cipherText[count]) + c - 65
+		if merje > 90 {
+			merje -= 25
+		}
+		newchar := string(merje)
+		result = result + newchar
+		count++
 	}
-
+	fmt.Println(result)
 }
