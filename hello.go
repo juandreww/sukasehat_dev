5c85d96d03521a6c4d70b99fdb3ac2902aed9ce1
juandreww <juandreww@outlook.com>
2021-12-15 22:58:28 +0700

    gorilla mux

--- hello.go
+++ hello.go
@@ -1,27 +1,47 @@
 package main
 
 import (
-	// Import the gorilla/mux library we just installed
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	// Declare a new router
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
 	r := mux.NewRouter()
-
-	// This is where the router is useful, it allows us to declare methods that
-	// this path will be valid for
 	r.HandleFunc("/hello", handler).Methods("GET")
 
-	// We can then pass our router (after declaring all our routes) to this method
-	// (where previously, we were leaving the second argument as nil)
-	http.ListenAndServe(":8080", r)
+	// Declare the static file directory and point it to the directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
-
